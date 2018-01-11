a877d90db3b79a30e1feee9d2bdbe9672a94846b
zngtfy <nvt87x@gmail.com>
2018-01-11 17:38:20 +0700

    Update route Product

--- routes.js
+++ routes.js
@@ -34,8 +34,8 @@ module.exports = function (app) {
         .get(product.list)
         .post(product.create);
 
-    app.route('/tasks/:id')
+    app.route('/products/:id')
         .get(product.read)
-        .put(product.update)
+        .patch(product.update)
         .delete(product.delete);
 };
