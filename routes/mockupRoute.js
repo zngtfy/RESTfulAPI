049ebcbc98beb32332451e87f40a5e596ad7b16e
zngtfy <nvt87x@gmail.com>
2018-01-23 17:28:32 +0700

    Add order-status API

--- routes/mockupRoute.js
+++ routes/mockupRoute.js
@@ -6,6 +6,7 @@ const ct = require("../controllers/mockupController");
 
 router.get("/dashboard-order-status", ct.dashboardOrderStatus);
 router.get("/dashboard-trade-status", ct.dashboardTradeStatus);
+router.get("/order-status", ct.dashboardOrderStatus);
 router.get("/trade-status", ct.tradeStatus);
 router.get("/trade-history-status", ct.historyStatus);
 router.get("/company-status", ct.companyStatus);
