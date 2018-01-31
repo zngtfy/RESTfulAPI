39ce8b22da468ef3ef23b9f5dcddcc19bb7218a7
zngtfy <nvt87x@gmail.com>
2018-01-31 17:59:55 +0700

    Remove check authentication

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,9 +4,9 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        let token = req.headers.authorization.split(" ")[1];
+        /*let token = req.headers.authorization.split(" ")[1];
         let decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = decoded;
+        req.userData = decoded;*/
         next();
     } catch (error) {
         return res.status(401).json({
