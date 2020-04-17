d11e32f9946c79898afa002a026ed3b56d93336c
Zng Tfy <nvt87x@gmail.com>
2020-04-17 19:46:02 +0700

    Test products

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,38 +8,38 @@ const auth = require("../middleware/checkAuth");
 const ct = require("../controllers/productController");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const dir = "./uploads";
-    fs.exists(dir, (exists) => {
-      if (exists) {
-        cb(null, dir);
-      }
-      else {
-        fs.mkdir(dir, err => cb(err, dir));
-      }
-    });
-  },
-  filename: function (req, file, cb) {
-    const t = "_";
-    cb(null, t + file.originalname);
-  }
+    destination: function (req, file, cb) {
+        const dir = "./uploads";
+        fs.exists(dir, (exists) => {
+            if (exists) {
+                cb(null, dir);
+            }
+            else {
+                fs.mkdir(dir, err => cb(err, dir));
+            }
+        });
+    },
+    filename: function (req, file, cb) {
+        const t = "_";
+        cb(null, t + file.originalname);
+    }
 });
 
 const filter = (req, file, cb) => {
-  // Reject a file
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    // Reject a file
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/gif") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: filter
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: filter
 });
 
 router.get("/", ct.list);
